b6da4b9bf34f1b0dcaeba0e406d7feee520b7db5
MarcoGarciaa <margar306@gmail.com>
2024-03-07 14:10:19 +0100

    ...

--- app/src/main/java/com/example/ucar_login/LogInActivity.kt
+++ app/src/main/java/com/example/ucar_login/LogInActivity.kt
@@ -4,13 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.example.ucar_login.databinding.ActivityLogInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.Firebase
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.FirebaseDatabase
 
 class LogInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogInBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private var RC_SIGN_IN = 20
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +33,27 @@ class LogInActivity : AppCompatActivity() {
 
         binding = ActivityLogInBinding.inflate(layoutInflater)
         val view = binding.root
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        val gso = GoogleSignIn.getClient(
+            this,
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        )
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
         setContentView(view)
 
         //LOGIN GOOGLE
         binding.btnLoginGoogle.setOnClickListener {
-            val intent = Intent(this, AuthenticationActivity::class.java)
-            startActivity(intent)
-
+            googleSignIn()
         }
+
         //LOGIN FACEBOOK
         binding.btnLoginFacebook.setOnClickListener {
             val intent = Intent(this, AuthenticationActivity::class.java)
@@ -42,4 +69,44 @@ class LogInActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    private fun googleSignIn() {
+
+        Intent intent = GoogleSignInClient.getSignInIntent();
+        startActivityForResult(intent,RC_SIGN_IN);
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                FirebaseAuth(account?.idToken)
+            } catch (e: ApiException) {
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    private fun firebaseAuth(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val map = HashMap<String, Any>()
+                    map["id"] = user!!.uid
+                    map["name"] = user.displayName!!
+                    map["profile"] = user.photoUrl.toString()
+
+                    database.reference.child("users").child(user.uid).setValue(map)
+                    val intent = Intent(this, SingInStep1Activity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Algo salió mal", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
