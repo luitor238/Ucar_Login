3573d1b9b2223735b5839d4aa57a43ad1dcb374a
luitor238 <luistorgacastro@gmail.com>
2024-03-13 13:24:48 +0100

    login y auth unido en uno

--- app/src/main/java/com/example/ucar_login/SignInStep3Activity.kt
+++ app/src/main/java/com/example/ucar_login/SignInStep3Activity.kt
@@ -5,38 +5,18 @@ import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
-import com.example.ucar_login.databinding.ActivitySignInStep2Binding
 import com.example.ucar_login.databinding.ActivitySignInStep3Binding
 
 class SignInStep3Activity : AppCompatActivity() {
-
-    private lateinit var btnGoBack: ImageButton
-    private lateinit var btnNext: Button
-
-    private lateinit var btnCamera: ImageButton
-    private lateinit var btnGallery: ImageButton
-
-    private lateinit var Image: ImageView
-    private lateinit var Name: EditText
-
-    private lateinit var TextViewResult: TextView
-
-    private lateinit var name: String
-
 
     private lateinit var binding: ActivitySignInStep3Binding
 
     val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){uri ->
         if(uri!=null){
-            Image.setImageURI(uri)
+            binding.imageUser.setImageURI(uri)
         }
     }
 
@@ -51,48 +31,44 @@ class SignInStep3Activity : AppCompatActivity() {
 
         val username = intent.getStringExtra("Username")
         val password = intent.getStringExtra("Password")
-        val email = binding.getStringExtra("Email")
-        val phoneNumber = binding.getStringExtra("PhoneNumber")
-
-        btnGoBack = findViewById(R.id.imageBtnGoBack3)
-        btnNext = findViewById(R.id.btnNext3)
-        btnCamera = findViewById(R.id.btnCamera)
-        btnGallery = findViewById(R.id.btnGallery)
-        Image = findViewById<ImageView>(R.id.imageUser)
-        Name = findViewById<EditText>(R.id.editTextName)
-        name = Name.toString()
-        TextViewResult = findViewById(R.id.textViewResult)
+        val email = intent.getStringExtra("Email")
+        val phoneNumber = intent.getStringExtra("PhoneNumber")
+        val image = binding.imageUser
+        val name = binding.editTextName.text.toString()
 
 
         //CAMERA BUTTON
-        btnCamera.setOnClickListener {
+        binding.btnCamera.setOnClickListener {
             camera()
         }
 
         //GALLERY BUTTON
-        btnGallery.setOnClickListener {
+        binding.btnGallery.setOnClickListener {
             pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
 
         //GO BACK BUTTON
-        btnGoBack.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
+        binding.imageBtnGoBack.setOnClickListener {
+            val intent = Intent(this, SignInStep2Activity::class.java)
             intent.putExtra("Username",username)
             intent.putExtra("Password",password)
             startActivity(intent)
         }
 
         //NEXT BUTTON
-        btnNext.setOnClickListener {
+        binding.btnNext.setOnClickListener {
             if(name.isNotEmpty()){
-                user.setImage(Image)
-                user.setName(name)
-                val intent = Intent(this, SingInStep2Activity::class.java)
-                intent.putExtra("User",user)
+                val intent = Intent(this, SignInStep4Activity::class.java)
+                intent.putExtra("Username",username)
+                intent.putExtra("Password",password)
+                intent.putExtra("Email",email)
+                intent.putExtra("PhoneNumber",phoneNumber)
+                intent.putExtra("Image",image)
+                intent.putExtra("Name",name)
                 startActivity(intent)
             } else {
-                TextViewResult.setTextColor(ContextCompat.getColor(this, R.color.warning))
-                TextViewResult.text = "You have to put your name."
+                binding.textViewResult.setTextColor(ContextCompat.getColor(this, R.color.warning))
+                binding.textViewResult.text = "You have to put your name."
             }
         }
     }
@@ -112,7 +88,7 @@ class SignInStep3Activity : AppCompatActivity() {
         if(requestCode == 1 && resultCode == RESULT_OK){
             val extras = Bundle(data.extras)
             val imgBitmap :Bitmap = extras.get("data") as Bitmap
-            Image.setImageBitmap(imgBitmap)
+            binding.imageUser.setImageBitmap(imgBitmap)
         }
     }
 }
